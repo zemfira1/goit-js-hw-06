194375086e8f3442f8dc411e3ae15adca6e5149b
zemfira1 <craftyzema@gmail.com>
2023-05-14 22:06:59 -0400

    task 10??????

--- js/task-10.js
+++ js/task-10.js
@@ -11,8 +11,54 @@
 //Все элементы должены иметь случайный цвет фона в формате HEX. 
 //Используй готовую функцию getRandomHexColor для получения цвета.
 
+
+//-------------------Здравствуйте! Подскажите, пожалуйста, что я делаю не так, 
+//-------------------почему не работает ? Хочу победить это задание))))
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const number = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const box = document.querySelector('#boxes');
+// console.log(number);
+// console.log(createBtn);
+// console.log(destroyBtn);
+//console.log(box);
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+let amount = 0;
+
+function createBoxes(amount) {
+  amount = Number(number.value);
+  let sizeCount = 30;
+   
+  for (let i = 1; i <= amount; i += 1){
+    let div = document.createElement('div');
+    div.backgroundColor = getRandomHexColor();
+    div.style.width = sizeCount;
+    div.style.height = sizeCount;
+
+    box.insertAdjacentHTML('beforeend', div);
+    sizeCount += 10;
+  }
+  console.log(box);
+  return;
+};
+ console.log(box);
+
+
+function destroyBoxes(event) {
+  box.innerHTML = '';
+};
+
+
+
+
